b1e8a16f321d79d9a0f6c7a8001c66dc1e729509
AmandaPersdotter <amanda.vivianne.p.l@hotmail.com>
2026-04-13 14:42:42 +0200

    css registrering

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Layout from "../../components/layout/Layout";
 import styles from "./Register.module.css";
+import heroImg from "../../assets/Hero Startsida.png";
 
 function Register() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -14,6 +16,7 @@ function Register() {
   const handleRegister = async () => {
     try {
       await axios.post("http://localhost:8080/api/auth/register", {
+        name,
         email,
         password,
       });
@@ -27,8 +30,18 @@ function Register() {
 
   return (
     <Layout>
+        <div className={styles.wrapper}>
+        <div className={styles.hero}>
+          <img src={heroImg} alt="hero" />
+        </div>
       <div className={styles.container}>
         <h2>Registrera</h2>
+
+        <input
+          placeholder="Namn"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <input
           placeholder="Email"
@@ -45,7 +58,8 @@ function Register() {
 
         <button onClick={handleRegister}>Registrera</button>
 
-        {error && <p>{error}</p>}
+        {error && <p className={styles.error}>{error}</p>}
+      </div>
       </div>
     </Layout>
   );
